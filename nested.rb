e6d29a477e219786fbaa1b151c41dc4bd988b4cd
Brad Carter <bradc1993@gmail.com>
2020-05-14 21:06:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
       }
     }
 
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
       }
     }
 
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -56,6 +58,7 @@ def dennis_ritchies_language
       }
     }
 
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
